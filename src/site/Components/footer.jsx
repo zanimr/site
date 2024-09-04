18c75103ddb875e033bddc1032906b3e9ba98779
zanimr <93678442+zanimr@users.noreply.github.com>
2024-09-04 09:34:43 -0300

    Alterar ícones do footer

--- src/site/Components/footer.jsx
+++ src/site/Components/footer.jsx
@@ -6,9 +6,9 @@ function Footer(){
     return <section id="footer">    
       <div>
             <ul className="list-unstyled list-inline social text-center">
-                <li className="list-inline-item"><a href="https://wa.me/17991849186?text=Olá, quero saber mais"><i className="fa fa-whatsapp fa-2x"></i></a></li>
-                <li className="list-inline-item"><a href="https://www.instagram.com/nhsoftware/"><i className="fa fa-instagram fa-2x"></i></a></li>						
-                <li className="list-inline-item"><a href="mailto:contato.nhsoftware@gmail.com" target="_blank"><i className="fa fa-envelope fa-2x"></i></a></li>
+                <li className="list-inline-item"><a href="https://wa.me/17991849186?text=Olá, quero saber mais"><i className="fa fa-whatsapp fa-4x"></i></a></li>
+                <li className="list-inline-item"><a href="https://www.instagram.com/nhsoftware/"><i className="fa fa-instagram fa-4x"></i></a></li>						
+                <li className="list-inline-item"><a href="mailto:contato.nhsoftware@gmail.com" target="_blank"><i className="fa fa-envelope fa-4x"></i></a></li>
             </ul>
 	    </div>
       <a href="mailto:contato.nhsoftware@gmail.com">contato.nhsoftware@gmail.com</a>

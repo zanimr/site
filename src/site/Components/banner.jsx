7940547ac1c238509c018a3e1a39adbc42086022
zanimr <93678442+zanimr@users.noreply.github.com>
2024-09-04 09:14:38 -0300

    Update banner.jsx

--- src/site/Components/banner.jsx
+++ src/site/Components/banner.jsx
@@ -8,8 +8,6 @@ function Banner(){
                 <div className="col-lg-6">
                     <h1>Desenvolvimento de aplicativos mobile para sua empresa.</h1>
                     <h4>Criamos o app ideal para sua empresa.</h4>
-                    <a href="/app/novaconta" className="btn btn-dark btn-lg btn-banner">Criar uma conta</a>
-                    <a href="/app" className="btn btn-outline-light btn-lg btn-banner">Fazer Login</a>
                 </div>
 
                 <div className="col-lg-6">

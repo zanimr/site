e250fe45f27c5510c232c54faeb6b9f37bbd9b43
zanimr <93678442+zanimr@users.noreply.github.com>
2024-09-12 07:06:29 -0300

    Alterando celular

--- src/site/Components/footer.jsx
+++ src/site/Components/footer.jsx
@@ -6,13 +6,13 @@ function Footer(){
     return <section id="footer">    
       <div>
             <ul className="list-unstyled list-inline social text-center">
-                <li className="list-inline-item"><a href="https://wa.me/17991849186?text=Olá, quero saber mais"><i className="fa fa-whatsapp fa-4x"></i></a></li>
+                <li className="list-inline-item"><a href="https://wa.me/17992523805?text=Olá, tenho interesse e quero saber mais"><i className="fa fa-whatsapp fa-4x"></i></a></li>
                 <li className="list-inline-item"><a href="https://www.instagram.com/nhsoftware/"><i className="fa fa-instagram fa-4x"></i></a></li>						
                 <li className="list-inline-item"><a href="mailto:contato.nhsoftware@gmail.com" target="_blank"><i className="fa fa-envelope fa-4x"></i></a></li>
             </ul>
 	    </div>
       <a href="mailto:contato.nhsoftware@gmail.com">contato.nhsoftware@gmail.com</a>
-      <p>WhatsApp (17) 99184-9186</p>
+      <p>WhatsApp (17) 99252 3805</p>
       <p>Desenvolvido por nhsoftware - {ano}</p>
 
     </section>;
